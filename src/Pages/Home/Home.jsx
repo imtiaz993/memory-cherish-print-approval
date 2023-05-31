2300315a0ec9bb0485b7eda86723771c5b930a4b
imtiaz hussain <imtiazh993@gmail.com>
2023-05-31 07:16:55 -0700

    initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-9xl text-center text-[#FBF4E3]">PRINT APPROVAL FORM</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
+   theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#29303D',
+      },
+    }
+  },
+  plugins: [],
+};
