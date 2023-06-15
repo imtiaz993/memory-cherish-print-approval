a02b22e1a1aaece4bd473945226cfcf03170923b
imtiaz hussain <imtiazh993@gmail.com>
2023-06-14 23:02:48 -0700

    scroll to top

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,10 +4,12 @@ import Home from "../Pages/Home/Home";
 import ProductDetails from "../Pages/Product/ProductDetails";
 import FramedPrints from "../Pages/FramedPrints/FramedPrints";
 import Order from "../Pages/Order/Order";
+import ScrollToTop from "../Common/ScrollToTop";
 
 const WebRoutes = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<ProductDetails />} />
